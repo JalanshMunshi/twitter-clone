fe5623db1f7d1980fbafd77e6163d4d35b2ae7ae
JalanshMunshi <jalansh.munshi1366@gmail.com>
2022-03-11 18:51:28 -0700

    First commit.

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React from 'react';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        Test
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
